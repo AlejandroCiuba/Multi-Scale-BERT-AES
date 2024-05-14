6814678a56fc3d1cf68004031908f8ee8805af14
Alejandro <alejandrociuba@gmail.com>
2024-05-14 15:26:12 -0400

    Modifications to work on local PC

--- decode.sh
+++ decode.sh
@@ -1,3 +1,3 @@
-python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -Ur requirements.txt
+# python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -Ur requirements.txt
 python predict_multi_scale_multi_loss.py
 
