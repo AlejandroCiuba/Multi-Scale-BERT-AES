c2bb51f99556e41e12b83b50ae37e339de78e13e
Alejandro <alejandrociuba@gmail.com>
2024-07-15 21:13:26 -0400

    Additional comments

--- finetuning/finetune.py
+++ finetuning/finetune.py
@@ -1,5 +1,8 @@
 # Finetune the model on a different prompt
 # Created by Alejandro Ciuba, alc307@pitt.edu
+# Ask about how to implement the loss function
+# Ask if we should retrain/train from scratch/finetune
+# Ask about how to train/finetune through HuggingFace
 import argparse
 
 
